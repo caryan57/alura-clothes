18adf63aaebe8a2c830d561ed8a9f3b0ef6e2173
Carlos Olivares <focus.olivares@gmail.com>
2022-08-29 12:27:53 -0500

    adding form validation to app.js for contact, login and products forms

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 import { hideWrappedElements } from './helpers/hideWrappedElements.js';
 import { manageDropImage } from './helpers/dropImage.js';
 import { scrollAnimation } from './helpers/animateAnchor.js';
+import { validateForm } from './helpers/validateForm.js';
 
 const galleries = document.querySelectorAll('.carrousel__gallery');
 
@@ -18,4 +19,26 @@ const heroBtnLink = document.querySelector('.hero__content__info-button');
 
 if (heroBtnLink) {
   scrollAnimation(heroBtnLink);
+}
+
+// Contact Validation
+const contactForm = document.querySelector('.footer__item-form');
+const contactInputs = contactForm.querySelectorAll('[data-form]');
+
+validateForm(contactInputs);
+
+// Login Validation
+const loginForm = document.querySelector('.login__form');
+
+if (loginForm) {
+  const loginInputs = loginForm.querySelectorAll('[data-form]');
+  validateForm(loginInputs);
+}
+
+// Products Validation
+const productsForm = document.querySelector('.createProducts__form');
+
+if (productsForm) {
+  const productsInputs = document.querySelectorAll('[data-input]');
+  validateForm(productsInputs);
 }
