14584cf9bbe14aa3f1dd058022750902b72bb48d
Carlos Olivares <focus.olivares@gmail.com>
2022-08-29 12:32:24 -0500

    adding product-services.js for managing CRUD

---
+++ js/service/products-service.js
@@ -0,0 +1,68 @@
+const url = 'http://localhost:3000/products';
+
+const getProducts = () =>
+  fetch(url, {
+    headers: { 'Content-Type': 'application/json' },
+    method: 'GET',
+  }).then(response => response.json());
+
+const getProduct = id =>
+  fetch(`${url}/${id}`).then(response => response.json());
+
+const registerProduct = (
+  imageUrl,
+  title,
+  category,
+  price,
+  stock,
+  description
+) =>
+  fetch(url, {
+    headers: { 'Content-Type': 'application/json' },
+    method: 'POST',
+    body: JSON.stringify({
+      id: uuid.v4(),
+      title,
+      category,
+      price,
+      stock,
+      imageUrl,
+      description,
+    }),
+  });
+
+const updateProduct = (
+  id,
+  imageUrl,
+  title,
+  category,
+  price,
+  stock,
+  description
+) => {
+  return fetch(`${url}/${id}`, {
+    headers: { 'Content-Type': 'application/json' },
+    method: 'PUT',
+    body: JSON.stringify({
+      id,
+      title,
+      category,
+      price,
+      stock,
+      imageUrl,
+      description,
+    }),
+  });
+};
+
+const deleteProduct = id => {
+  return fetch(`${url}/${id}`, { method: 'DELETE' });
+};
+
+export const productsServices = {
+  getProducts,
+  getProduct,
+  registerProduct,
+  updateProduct,
+  deleteProduct,
+};
